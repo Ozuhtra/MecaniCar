432baa445190e50bc724a17fad20db7efcac8442
Pedro Michelin <pedroarthuzo1110@gmail.com>
2026-03-06 21:34:54 -0300

    Novas funções e correções de bugs
    Agora é possível converter de Nm para Kgfm e vice-versa, além de serem reparadas correções de texto e UX/UI

--- MecaniCar.py
+++ MecaniCar.py
@@ -1,15 +1,54 @@
-import tkinter as tk
+import tkinter as tk    # Bibliotecas
 from tkinter import messagebox, simpledialog, PhotoImage
+import webbrowser
 
-# --- FUNÇÕES DE LÓGICA E CÁLCULO ---
-
-def fechar_janela():
+def fechar_janela():    # Confirma sair do app
     if messagebox.askyesno("Confirmação", "Deseja mesmo sair do MecaniCar?"):
         janela.destroy()
 
-def conversor_forca():
+def conversor_forca():  # Conversor NM para KGFM
     janela_opcoes = tk.Toplevel(janela)
     janela_opcoes.title("Conversor de Força")
+    janela_opcoes.geometry("300x200")
+    janela_opcoes.configure(bg="#2c3e50")
+
+    janela_opcoes.transient(janela)
+
+    tk.Label(janela_opcoes, text="Selecione a conversão:",
+             fg="white", bg="#2c3e50", font=("Arial", 10, "bold")).pack(pady=15)
+    
+    def executar_calculo(modo):
+        if modo == "NM_KGFM":
+            valor = simpledialog.askfloat("MecaniCar", "Digite o valor em NM", minvalue=0.1)
+            if valor is not None:
+                resultado = valor * 0.101972
+                mensagem_nm = f"""
+                Relatório MecaniCar
+                ---------------------------------------------------
+                Resultado: {valor:.2f} NM = {resultado:.2f} KGFM"""
+
+                messagebox.showinfo("Diagnóstico", mensagem_nm)
+                janela_opcoes.destroy()
+        else:
+                valor = simpledialog.askfloat("MecaniCar", "Digite o valor em KGFM", minvalue=0.1)
+                if valor is not None:
+                    resultado = valor * 9.8067
+                    mensagem_kgfm = f"""
+                Relatório MecaniCar
+                ---------------------------------------------------
+                Resultado: {valor:.2f} KGFM = {resultado:.2f} NM"""
+                    
+                messagebox.showinfo("Diagnóstico", mensagem_kgfm)
+        
+    tk.Button(janela_opcoes, text="NM -> KGFM", width=20, bg="#061824", fg="white",
+              command=lambda: executar_calculo("NM_KGFM")).pack(pady=5)
+    
+    tk.Button(janela_opcoes, text="KGFM -> NM", width=20, bg="#061824", fg="white",
+              command=lambda: executar_calculo("KGFM_NM")).pack(pady=5)
+
+def conversor_potencia():   # Conversor CV para HP
+    janela_opcoes = tk.Toplevel(janela)
+    janela_opcoes.title("Conversor de Potência")
     janela_opcoes.geometry("300x200")
     janela_opcoes.configure(bg="#2c3e50")
 
@@ -23,17 +62,24 @@ def conversor_forca():
             valor = simpledialog.askfloat("MecaniCar", "Digite o valor em HP:", minvalue=0.0)
             if valor is not None:
                 resultado = valor * 1.01387
-                messagebox.showinfo("Relatório MecaniCar \n"
-                                    "--------------------------\n"
-                                    "Resultado", f"{valor:.2f} HP = {resultado:.2f} CV")
+                mensagem_hp = f"""
+                Relatório MecaniCar
+                ---------------------------------------------------
+                Resultado: {valor:.2f} HP = {resultado:.2f} CV"""
+                
+                messagebox.showinfo("Diagnóstico", mensagem_hp)
+
                 janela_opcoes.destroy()
         else:
             valor = simpledialog.askfloat("MecaniCar", "Digite o valor em CV:", minvalue=0.0)
             if valor is not None:
                 resultado = valor * 0.98632
-                messagebox.showinfo("Relatório MecaniCar \n"
-                                    "--------------------------\n"
-                                    "Resultado", f"{valor:.2f} CV = {resultado:.2f} HP")
+                mensagem_cv = f"""
+                Relatório MecaniCar
+                ---------------------------------------------------
+                Resultado: {valor:.2f} CV = {resultado:.2f} HP"""
+                
+                messagebox.showinfo("Diagnóstico", mensagem_cv)
 
     tk.Button(janela_opcoes, text="HP -> CV", width=20, bg="#061824", fg="white",
               command=lambda: executar_calculo("HP_CV")).pack(pady=5)
@@ -41,8 +87,7 @@ def conversor_forca():
     tk.Button(janela_opcoes, text="CV -> HP", width=20, bg="#061824", fg="white",
               command=lambda: executar_calculo("CV_HP")).pack(pady=5)
 
-def conversor_pressao():
-    """Cria uma janelinha de escolha para o mecânico"""
+def conversor_pressao():    # Conversor BAR para PSI
     janela_opcoes = tk.Toplevel(janela)
     janela_opcoes.title("Conversão de Pressão")
     janela_opcoes.geometry("300x200")
@@ -59,29 +104,36 @@ def conversor_pressao():
             valor = simpledialog.askfloat("MecaniCar", "Digite o valor em BAR:", minvalue=0.0)
             if valor is not None:
                 resultado = valor * 14.5038
-                messagebox.showinfo("Relatório MecaniCar \n"
-                                    "--------------------------\n"
-                                    "Resultado", f"{valor:.2f} BAR = {resultado:.2f} PSI"
-                                    "O valor padrão para pneus de carro é entre 30 a 35 PSI")
+                mensagem_bar = f"""
+                Relatório MecaniCar
+                ---------------------------------------------------
+                Resultado: {valor:.2f} BAR = {resultado:.2f} PSI
+                O valor padrão para pneus de carro é entre 30 a 35 PSI"""
+                
+                messagebox.showinfo("Diagnóstico", mensagem_bar)
                 janela_opcoes.destroy()
         else:
             valor = simpledialog.askfloat("MecaniCar", "Digite o valor em PSI:", minvalue=0.0)
             if valor is not None:
                 resultado = valor * 0.0689476
-                messagebox.showinfo("Relatório Mecanicar \n"
-                                    "--------------------------\n"
-                                    "Resultado", f"{valor:.2f} PSI = {resultado:.2f} BAR"
-                                    "O valor padrão para pneus de carro é entre 2 e 2,4 BAR")
+                mensagem_psi = f"""
+            Relatório Mecanicar
+            ---------------------------------------------------
+            Resultado: {valor:.2f} PSI = {resultado:.2f} BAR
+            O valor padrão para pneus de carro é entre 2 e 2,4 BAR"""
+                
+                messagebox.showinfo("Diagnóstico", mensagem_psi)
                 janela_opcoes.destroy()
-
-    # Botões de opção
-    tk.Button(janela_opcoes, text="BAR -> PSI", width=20, bg="#061824", fg="white",
-              command=lambda: executar_calculo("BAR_PSI")).pack(pady=5)
     
+    tk.Button(janela_opcoes, text="BAR -> PSI", width=20, bg="#061824", fg="white",
+
+              command=lambda: executar_calculo("BAR_PSI")).pack(pady=5)
+
     tk.Button(janela_opcoes, text="PSI -> BAR", width=20, bg="#061824", fg="white",
+
               command=lambda: executar_calculo("PSI_BAR")).pack(pady=5)
 
-def calcular_dinamico(entradas, janela_calculo, tensao):
+def calcular_dinamico(entradas, janela_calculo, tensao):    # Calcula circuito série
     try:
         valores = [float(en.get().replace(',', '.')) for en in entradas if en.get()]
         if not valores: return
@@ -97,14 +149,15 @@ def calcular_dinamico(entradas, janela_calculo, tensao):
             f"Resistência Total: {req:.2f} Ω\n"
             f"Corrente: {corrente:.2f} A\n"
             f"Fusível Sugerido: {fusivel1:.2f} a {fusivel2:.2f} A\n"
-            f"Potência Total: {potencia:.2f} W"
+            f"Potência Total: {potencia:.2f} W\n"
+            f"Tensão do circuito: {tensao} V"
         )
         messagebox.showinfo("Diagnóstico", resultado)
         janela_calculo.destroy() 
     except ValueError:
         messagebox.showerror("Erro", "Preencha os campos com números válidos.")
 
-def calcular_paralelo_logica(entradas, janela_calculo, tensao):
+def calcular_paralelo_logica(entradas, janela_calculo, tensao): # Calcula circuito paralelo
     try:
         valores = [float(en.get().replace(',', '.')) for en in entradas if en.get()]
         if not valores: return
@@ -122,7 +175,8 @@ def calcular_paralelo_logica(entradas, janela_calculo, tensao):
             f"Resistência Total: {req_paralelo:.2f} Ω\n"
             f"Corrente: {corrente:.2f} A\n"
             f"Fusível Sugerido: {fusivel1:.2f} a {fusivel2:.2f} A\n"
-            f"Potência Total: {potencia:.2f} W"
+            f"Potência Total: {potencia:.2f} W\n"
+            f"Tensão do circuito: {tensao} V"
         )
         messagebox.showinfo("Diagnóstico", resultado)
         janela_calculo.destroy() 
@@ -131,15 +185,16 @@ def calcular_paralelo_logica(entradas, janela_calculo, tensao):
     except ValueError:
         messagebox.showerror("Erro", "Preencha os campos com números válidos.")
 
-# --- FUNÇÕES PARA ABRIR JANELAS DE CIRCUITOS ---
+def abrir_github(): # Abre o Github através do botão
+    webbrowser.open("https://github.com/Ozuhtra/MecaniCar")
 
-def abrir_janela_serie():
+def abrir_janela_serie():   # Abre a janela p/ calcular o paralelo
     configurar_janela(calcular_dinamico, "Série")
 
-def abrir_janela_paralelo():
+def abrir_janela_paralelo():    # Abre a janela p/ calcular o série
     configurar_janela(calcular_paralelo_logica, "Paralelo")
 
-def configurar_janela(funcao_calculo, tipo):
+def configurar_janela(funcao_calculo, tipo):    # Configurações da janela
     try:
         tensao = simpledialog.askfloat("MecaniCar", "Tensão do sistema (V):", minvalue=0.1)
         if tensao is None: return
@@ -161,26 +216,23 @@ def configurar_janela(funcao_calculo, tipo):
     except Exception as e:
         messagebox.showerror("Erro", f"Problema: {e}")
 
-# --- CONFIGURAÇÃO DA JANELA PRINCIPAL ---
-
-janela = tk.Tk()
+janela = tk.Tk()    # Configurações janela principal
 janela.title("MecaniCar")
 janela.geometry("1200x800")
 janela.protocol("WM_DELETE_WINDOW", fechar_janela)
 
-try:
+try:    # Plano de fundo
     img_fundo = PhotoImage(file="mecanicar.png") 
     lbl_fundo = tk.Label(janela, image=img_fundo)
     lbl_fundo.place(x=0, y=0, relwidth=1, relheight=1)
 except:
     janela.config(bg="#2c3e50")
 
-# Título
-lbl_titulo = tk.Label(janela, text="MECANICAR - A SUA SOLUÇÃO AUTOMOTIVA", 
+lbl_titulo = tk.Label(janela, text="MECANICAR - A SUA SOLUÇÃO AUTOMOTIVA",  # Textos da página principal
                       font=("Helvetica", 16, "bold"), fg="white", bg="#2c3e50")
 lbl_titulo.place(x=680, y=50, anchor="center")
 
-# Botões Lado Esquerdo (Circuitos)
+# ----- BOTÕES -----
 btn_iniciar_serie = tk.Button(janela, text="Calcular circuito em série", 
                         command=abrir_janela_serie, 
                         width=25, height=2, bg="#061824", fg="white", font=("Arial", 12, "bold"))
@@ -191,21 +243,29 @@ btn_iniciar_paralelo = tk.Button(janela, text="Calcular circuito em paralelo",
                         width=25, height=2, bg="#061824", fg="white", font=("Arial", 12, "bold"))
 btn_iniciar_paralelo.place(x=120, y=470, anchor="center")
 
-# Botão Lado Direito (Pressão)
 btn_calcular_pressao = tk.Button(janela, text="Calcular pressão", 
                                  command=conversor_pressao,
                                  width=25, height=2, bg="#061824", fg="white", font=("Arial", 12, "bold"))
-btn_calcular_pressao.place(x=950, y=440) # Ajustei levemente o X para caber na tela padrão
+btn_calcular_pressao.place(x=1110, y=440)
+
+btn_calcular_potencia = tk.Button(janela, text="Calcular potência",
+                               command=conversor_potencia,
+                               width=25, height=2, bg="#061824", fg="white", font=("Arial", 12, "bold"))
+btn_calcular_potencia.place(x=1110, y=388.5)
 
 btn_calcular_forca = tk.Button(janela, text="Calcular força",
                                command=conversor_forca,
                                width=25, height=2, bg="#061824", fg="white", font=("Arial", 12, "bold"))
-btn_calcular_forca.place(x=950, y=388.5)
-# Botão Sair
+btn_calcular_forca.place(x=1110, y=340)
+
+btn_versao_app = tk.Button(janela, text="v.0.0.2b",
+                           command=abrir_github,
+                           fg="red", font=("Arial", 12, "bold"))
+btn_versao_app.place(x=1290, y=680)
+
 btn_sair = tk.Button(janela, text="Fechar", command=fechar_janela, fg="red")
 btn_sair.place(x=680, y=650, anchor="center")
 
-# Botões de Créditos
 def sobre_eu():
     messagebox.showinfo("Pedro Michelin", "Sou o Pedro, amante de carros, eletroeletrônica, programação e futuro Engenheiro Elétrico!.")
 
